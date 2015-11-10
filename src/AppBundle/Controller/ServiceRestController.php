5d860d282a1b1e22ccb779a5a5f6e2ccaf47ee2b
Alexandre Martinez <alexandre.martinez76@gmail.com>
2015-11-10 17:33:28 +0100

    Fourniture url service

--- src/AppBundle/Controller/ServiceRestController.php
+++ src/AppBundle/Controller/ServiceRestController.php
@@ -33,6 +33,7 @@ class ServiceRestController extends Controller {
     public function getProviderUrlAction($name) {
        $service = $this->findService($name);
        $provider = array_rand($service->getProviders());
+       return $provider;
        if (!$provider) {
             throw $this->createNotFoundException("No known provider for $name");
         }
