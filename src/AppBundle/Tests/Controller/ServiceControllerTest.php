f751c970c0739343a104744fb6124be938b94310
Alexandre Martinez <alexandre.martinez76@gmail.com>
2015-11-19 14:26:25 +0100

    Added travis

--- src/AppBundle/Tests/Controller/ServiceControllerTest.php
+++ src/AppBundle/Tests/Controller/ServiceControllerTest.php
@@ -2,36 +2,87 @@
 
 namespace AppBundle\Tests\Controller;
 
+use AppBundle\Document\Provider;
+use AppBundle\Document\Service;
+use Doctrine\ODM\MongoDB\DocumentManager;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class ServiceControllerTest extends WebTestCase
-{
-    public function testIndex()
-    {
-        $client = static::createClient();
+class ServiceControllerTest extends WebTestCase {
 
-        $crawler = $client->request('GET', '/index');
+    public function setup() {
+        $this->client = static::createClient();
+        $this->removeServices();
     }
 
-    public function testRegister()
-    {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/register');
+    /**
+     * @test
+     */
+    public function getServicesReturnsEmptyListOfServiceNamesIfNoServices() {
+        $this->client->request('GET', '/api/services');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $actual = $this->client->getResponse()->getContent();
+        $this->assertJsonContent([], $actual);
     }
 
-    public function testUnregister()
-    {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/unregister');
+    /**
+     * @test
+     */
+    public function getServicesReturnsListOfServicesIfServices() {
+        $this->createServices([
+            ['name' => 'ServiceA'],
+            ['name' => 'ServiceB']
+        ]);
+        $this->client->request('GET', '/api/services');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $actual = $this->client->getResponse()->getContent();
+        $this->assertJsonContent([['name' => 'ServiceA'], ['name' => 'ServiceB']], $actual);
     }
 
-    public function testRetrieve()
-    {
-        $client = static::createClient();
+    /**
+     * @test
+     */
+    public function getServicesReturnsListOfServiceNamesIfServicesWithProviders() {
+        $this->createServices([
+            ['name' => 'ServiceA', 'providers' => ['http://test.example.com']],
+            ['name' => 'ServiceB']
+        ]);
+        $this->client->request('GET', '/api/services');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $actual = $this->client->getResponse()->getContent();
+        $this->assertJsonContent([['name' => 'ServiceA'], ['name' => 'ServiceB']], $actual);
+    }
 
-        $crawler = $client->request('GET', '/retrieve');
+    private function assertJsonContent($expected, $actual) {
+        $this->assertJson($actual);
+        $actualJson = json_decode($actual, true);
+        $this->assertEquals($expected, $actualJson);
+    }
+
+    private function removeServices() {
+        /* @var $odm DocumentManager */
+        $odm = $this->client->getContainer()->get('doctrine_mongodb')->getManager();
+        $odm->getDocumentCollection('AppBundle\Document\Service')->drop();
+    }
+
+    private function createServices($param0) {
+        /* @var $odm DocumentManager */
+        $odm = $this->client->getContainer()->get('doctrine_mongodb')->getManager();
+        foreach ($param0 as $serviceData) {
+            $service = $this->createService($serviceData);
+            $odm->persist($service);
+        }
+        $odm->flush();
+    }
+
+    private function createService($serviceData) {
+        $service = new Service($serviceData['name']);
+        if (isset($serviceData['providers'])) {
+            foreach ($serviceData['providers'] as $url) {
+                $provider = new Provider($url);
+                $service->addProvider($provider);
+            }
+        }
+        return $service;
     }
 
 }
